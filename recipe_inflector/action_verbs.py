db08fda32a41b35db8a74b093d79e8224019e48a
maticpeprnyx <126096627+maticpeprnyx@users.noreply.github.com>
2023-12-09 18:20:50 +0100

    extract multiverb phrases with nechat, e.g. necháme vychladnout

--- recipe_inflector/action_verbs.py
+++ recipe_inflector/action_verbs.py
@@ -4,8 +4,15 @@
 import sys
 from open_file import open_bare
 from APIs import brno_tagger
-from tag import is_structural
+from tag import is_structural, is_infinitive
 from verb_tags import get_action_verb_tags
+
+def find_closest_infinitive(vertical: list[list[str]], starting_index: int, right_context_size: int) -> list[str]:
+    for i in range(starting_index, starting_index + right_context_size):
+        if is_infinitive(vertical[i][2]):
+            return vertical[i]
+
+    return ["", "", ""]
 
 def main():
     if len(sys.argv) != 2:
@@ -24,12 +31,20 @@ def main():
 
         action_verb_tags: list[str] = get_action_verb_tags(tagged_text)
 
-        for token in tagged_text:
-            if not is_structural(token):
-                current_tag: str = token[2]
+        for i in range(len(tagged_text)):
+            if not is_structural(tagged_text[i]):
+                current_tag: str = tagged_text[i][2]
 
-                current_lemma: str = token[1].lower()
-                current_token: str = token[0].lower()
+                current_lemma: str = tagged_text[i][1].lower()
+                current_token: str = tagged_text[i][0].lower()
+
+                if current_lemma == "nechat":
+                    closest_verb = find_closest_infinitive(tagged_text, i, 3)
+                    current_lemma += " " + closest_verb[1].lower()
+                    current_token += " " + closest_verb[0].lower()
+
+                    current_lemma = current_lemma.strip()
+                    current_token = current_token.strip()
 
                 if current_tag in action_verb_tags:
                     print(f"{current_lemma}\t{current_token}")
