373784805f08ddd8bb658f65baca76944354b8ee
maticpeprnyx <126096627+maticpeprnyx@users.noreply.github.com>
2023-12-15 12:51:34 +0100

    delete corpus_probe

--- corpus_probe/process.ps1
+++
@@ -1,6 +0,0 @@
-foreach ($fullpath in Get-ChildItem -Path .\recepty\*.txt) {
-    $filename = $fullpath.Name
-    $outputfilename = "označkované\$filename"
-    python kód/tagger.py $fullpath.FullName | Out-File -FilePath $outputfilename
-    Start-Sleep -Seconds 1
-} 
